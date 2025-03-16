8edbd12d7467ffb0d7db49fa0151d89c419837ad
doh6077 <kimdohee@sheridancollege.ca>
2025-03-16 15:23:09 -0400

    First Commit

---
+++ Capstone_0310/src/main/java/ca/sheridancollege/kimdohee/MqttSubscriber.java
@@ -0,0 +1,50 @@
+package ca.sheridancollege.kimdohee;
+
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MqttSubscriber {
+	public static void main(String[] args) {
+        String broker = "tcp://10.23.169.55:1883"; // Example: "tcp://192.168.1.100:1883"
+        String topic = "test/topic";              
+        String clientId = "JavaSubscriber";
+ 
+        try {
+            MqttClient client = new MqttClient(broker, clientId);
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+ 
+            System.out.println("Connecting to broker: " + broker);
+            client.connect(options);
+            System.out.println("Connected");
+ 
+            client.subscribe(topic);
+            System.out.println("Subscribed to topic: " + topic);
+ 
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.out.println("Connection lost: " + cause.getMessage());
+                }
+ 
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    System.out.println("Message received:");
+                    System.out.println("\tTopic: " + topic);
+                    System.out.println("\tMessage: " + new String(message.getPayload()));
+                }
+ 
+                @Override
+                public void deliveryComplete(org.eclipse.paho.client.mqttv3.IMqttDeliveryToken token) {
+                    // Not used for subscriber
+                }
+            });
+ 
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
