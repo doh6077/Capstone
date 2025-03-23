93b0a2b03cebb9b716acd1a0f4d02f93419c6e20
doh6077 <kimdohee@sheridancollege.ca>
2025-03-23 15:35:27 -0400

    add SensorController with GET and POST endpoints

---
+++ Capstone_0310/src/main/java/ca/sheridancollege/smartwaste/web/rest/SensorConroller.java
@@ -0,0 +1,41 @@
+package ca.sheridancollege.smartwaste.web.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.sheridancollege.smartwaste.beans.Sensor;
+import ca.sheridancollege.smartwaste.repositories.SensorRepository;
+import ca.sheridancollege.smartwaste.services.SensorService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/sensors")
+@AllArgsConstructor
+public class SensorConroller {
+
+	private SensorService sensorService;
+    // Get all sensors
+    @GetMapping("")
+    public List<Sensor> getSensors() {
+        return sensorService.findAll();
+    }
+
+    // Get a sensor by ID
+    @GetMapping("/{id}")
+    public Sensor getSensorById(@PathVariable Long id) {
+        return sensorService.findById(id);
+    }
+
+    // Add a new sensor
+    @PostMapping(value = {"/"}, headers = {"Content-type=application/json"})
+    public Sensor postSensor(@RequestBody Sensor sensor) {
+        sensor.setId(null);  
+        return sensorService.save(sensor);
+    }
+}
