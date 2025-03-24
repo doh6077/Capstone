48122b40a44262c487c4f46ad47c8e2610e93696
doh6077 <kimdohee@sheridancollege.ca>
2025-03-24 11:46:35 -0400

    Added Capstone_0310 project

--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/repositories/SensorRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.smartwaste.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ca.sheridancollege.smartwaste.beans.Sensor;
-
-public interface SensorRepository extends JpaRepository<Sensor, Long> {
-
-}
