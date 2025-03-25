9d6512faf5a38d73d812c33621745259d8cec399
doh6077 <kimdohee@sheridancollege.ca>
2025-03-25 11:23:20 -0400

    Displayed sensor list with sample data in table

--- JavaV17_Server/src/main/webapp/src/app/sensors/sensor-list/sensor-list.component.ts
+++ JavaV17_Server/src/main/webapp/src/app/sensors/sensor-list/sensor-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Sensor } from '../sensor';
 
 @Component({
   selector: 'app-sensor-list',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './sensor-list.component.css'
 })
 export class SensorListComponent {
-
+  sensors: Sensor[]=[];
+  ngOnInit():void{
+  this.sensors = [
+  {id: 1, batteryStatus: 'High', lastBatteryUpdateDate:"Mar 25", location:"A building"},
+  {id: 2, batteryStatus: 'Low', lastBatteryUpdateDate:"Mar 26", location:"B building"},
+  {id: 3, batteryStatus: 'Low', lastBatteryUpdateDate:"Mar 27", location:"C building"},
+  ];
+  }
 }
