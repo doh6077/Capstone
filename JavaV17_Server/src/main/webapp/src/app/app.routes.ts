4d1e0462409fd46d100433796439710549e4b753
doh6077 <kimdohee@sheridancollege.ca>
2025-03-25 11:30:52 -0400

    Added routing for SensorListComponent and updated SensorsComponent to use router outlet

--- JavaV17_Server/src/main/webapp/src/app/app.routes.ts
+++ JavaV17_Server/src/main/webapp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SensorListComponent } from './sensors/sensor-list/sensor-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {'path':'view', component:SensorListComponent}
+
+];
--- JavaV17_Server/src/main/webapp/src/app/sensors/sensors.component.ts
+++ JavaV17_Server/src/main/webapp/src/app/sensors/sensors.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { SensorListComponent } from './sensor-list/sensor-list.component';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-sensors',
-  imports: [SensorListComponent],
+  imports: [SensorListComponent, RouterOutlet],
   templateUrl: './sensors.component.html',
   styleUrl: './sensors.component.css'
 })
