5834c365ff693eeee7b4cc4ca9d00e650a1a6471
doh6077 <kimdohee@sheridancollege.ca>
2025-03-25 11:15:33 -0400

    Added SensorList component to panel layout and display sample sensor items

--- JavaV17_Server/src/main/webapp/src/app/sensors/sensors.component.ts
+++ JavaV17_Server/src/main/webapp/src/app/sensors/sensors.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SensorListComponent } from './sensor-list/sensor-list.component';
 
 @Component({
   selector: 'app-sensors',
-  imports: [],
+  imports: [SensorListComponent],
   templateUrl: './sensors.component.html',
   styleUrl: './sensors.component.css'
 })
