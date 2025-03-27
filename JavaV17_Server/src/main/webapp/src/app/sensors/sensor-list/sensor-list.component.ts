692ebc9a45bd2027670936780e4f32b42424a0b7
doh6077 <kimdohee@sheridancollege.ca>
2025-03-26 21:44:13 -0400

    Connected frontend to REST API to display database records

--- JavaV17_Server/src/main/webapp/src/app/sensors/sensor-list/sensor-list.component.ts
+++ JavaV17_Server/src/main/webapp/src/app/sensors/sensor-list/sensor-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Sensor } from '../sensor';
 import { RouterLink } from '@angular/router';
+import { SensorService } from '../sensor.service';
 @Component({
   selector: 'app-sensor-list',
   imports: [ RouterLink],
@@ -9,11 +10,27 @@ import { RouterLink } from '@angular/router';
 })
 export class SensorListComponent {
   sensors: Sensor[]=[];
+  //Update the list of students with the web service info.
+  getSensors(): void{
+    this.sensorService.getAll().subscribe({
+    next: (data) =>{
+    this.sensors=data;
+    }
+    });
+    }
+  //Connect to the Web Service
+  constructor(private sensorService:SensorService){}
   ngOnInit():void{
+    this.getSensors();
+
+
+    /*
   this.sensors = [
   {id: 1, batteryStatus: 'High', lastBatteryUpdateDate:"Mar 25", location:"A building"},
   {id: 2, batteryStatus: 'Low', lastBatteryUpdateDate:"Mar 26", location:"B building"},
   {id: 3, batteryStatus: 'Low', lastBatteryUpdateDate:"Mar 27", location:"C building"},
   ];
+
+  */
   }
 }
