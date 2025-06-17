24145b25dba68356f3def2e80aa2e244ca7d5cb4
doh6077 <kimdohee@sheridancollege.ca>
2025-06-17 14:27:30 -0400

    Update app routes to include cleaners and sensor history views

--- JavaV17_Server/src/main/webapp/src/app/app.routes.ts
+++ JavaV17_Server/src/main/webapp/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { SensorListComponent } from './sensors/sensor-list/sensor-list.component';
 import { SensorsComponent } from './sensors/sensors.component';
 import { SensorHistoryComponent } from './sensors/sensor-history/sensor-history.component';
+import { CleanerAddComponent } from './cleaners/cleaner-add/cleaner-add.component';
+import { CleanerListComponent } from './cleaners/cleaner-list/cleaner-list.component';
+import { CleanersComponent } from './cleaners/cleaners.component';
 export const routes: Routes = [
 
-    {'path':'', component:SensorsComponent},
-    {'path':'view', component:SensorListComponent},
-    { path: 'view/:id/history', component: SensorHistoryComponent }
+    {'path':'sensors', component:SensorsComponent},
+    {'path':'cleaners', component:CleanersComponent},
+    {'path':'view/sensors', component:SensorListComponent},
+    { 'path' :'view/sensors/:id/history', component: SensorHistoryComponent },
+    { path: 'view/cleaners', component: CleanerListComponent },
+    { path: 'add/cleaners', component: CleanerAddComponent },
 
 
 ];
