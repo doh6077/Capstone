46f7a4a75f093e47dae582cba03f48125de01cba
Sichao Quan <quansi@sheridancollege.ca>
2025-06-19 15:29:28 -0400

    test email notification functionality for full trash bin

--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/bootstrap/SensorBootstrap.java
+++ JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/bootstrap/SensorBootstrap.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 import ca.sheridancollege.smartwaste.beans.Sensor;
 import ca.sheridancollege.smartwaste.services.SensorService;
 import lombok.AllArgsConstructor;
+
 @Component
 @AllArgsConstructor
 public class SensorBootstrap implements CommandLineRunner {
-	
+
 	private SensorService sensorService;
 
 	@Override
@@ -17,6 +18,10 @@ public class SensorBootstrap implements CommandLineRunner {
 		Sensor sensor1 = Sensor.builder().macAddress("34:b7:da:5d:80:0c").trigerPin(7).echoPin(8).build();
 		Sensor sensor2 = Sensor.builder().macAddress("34:b7:da:5d:80:0c").trigerPin(12).echoPin(13).build();
 		Sensor sensor3 = Sensor.builder().macAddress("34:b7:da:5d:80:0c").trigerPin(2).echoPin(4).build();
+		// Sensor sensor10 =
+		// Sensor.builder().macAddress("00:11:22:33:44:55").trigerPin(10).echoPin(10).threshold(90.0f)
+		// .build();
+
 		sensorService.save(sensor1);
 		sensorService.save(sensor2);
 		sensorService.save(sensor3);
--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/TrashBinServiceImpl.java
+++ JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/TrashBinServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import ca.sheridancollege.smartwaste.beans.Cleaner;
+import ca.sheridancollege.smartwaste.beans.Sensor;
 import ca.sheridancollege.smartwaste.beans.TrashBin;
 import ca.sheridancollege.smartwaste.beans.TrashBinLocation;
 import ca.sheridancollege.smartwaste.beans.TrashBinType;
@@ -32,22 +33,21 @@ public class TrashBinServiceImpl implements TrashBinService {
         return trashBinRepository.save(trashBin);
     }
 
-  @Override
-public TrashBin update(Long id, TrashBin updatedBin) {
-    return trashBinRepository.findById(id).map(existingBin -> {
-        existingBin.setName(updatedBin.getName());
-        existingBin.setHeight(updatedBin.getHeight());
-        existingBin.setCreatedDate(updatedBin.getCreatedDate());
+    @Override
+    public TrashBin update(Long id, TrashBin updatedBin) {
+        return trashBinRepository.findById(id).map(existingBin -> {
+            existingBin.setName(updatedBin.getName());
+            existingBin.setHeight(updatedBin.getHeight());
+            existingBin.setCreatedDate(updatedBin.getCreatedDate());
 
-        existingBin.setSensor(updatedBin.getSensor());             // Update sensor
-        existingBin.setType(updatedBin.getType());                 // Update bin type
-        existingBin.setLocation(updatedBin.getLocation());         // Update location
-        existingBin.setCleaners(updatedBin.getCleaners());         // Update list of assigned cleaners
+            existingBin.setSensor(updatedBin.getSensor()); // Update sensor
+            existingBin.setType(updatedBin.getType()); // Update bin type
+            existingBin.setLocation(updatedBin.getLocation()); // Update location
+            existingBin.setCleaners(updatedBin.getCleaners()); // Update list of assigned cleaners
 
-        return trashBinRepository.save(existingBin);
-    }).orElse(null);
-}
-
+            return trashBinRepository.save(existingBin);
+        }).orElse(null);
+    }
 
     @Override
     public void delete(Long id) {
@@ -68,4 +68,5 @@ public TrashBin update(Long id, TrashBin updatedBin) {
     public List<TrashBin> findByCleaner(Cleaner cleaner) {
         return trashBinRepository.findByCleaners(cleaner);
     }
+
 }
